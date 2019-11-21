6e8e67d83465ddbff23d4665a673bc923e911f21
Pedro Costa <pedro.costa@thetrainline.com>
2019-11-21 18:41:24 +0000

    minor code improvement

--- src/components/Pad/index.js
+++ src/components/Pad/index.js
@@ -5,79 +5,87 @@ import { Col, Row, Grid } from 'react-native-easy-grid';
 import Button from '../Button';
 import styles from './styles';
 
-// TODO: withProps
+const Pad = ({ onNumberPress, onOperatorPress, onResult, onDelete, onLongDelete }) => {
+  const NumberButton = ({ title }) => (
+    <Button style={styles.numberButtons} onPress={onNumberPress} title={title} />
+  );
 
-const Pad = ({ onNumberPress, onOperatorPress, onResult, onDelete, onLongDelete }) => (
-  <Grid style={styles.container}>
-    <Col>
-      <Row>
-        <Button style={styles.numberButtons} onPress={onNumberPress} title="7" />
-      </Row>
-      <Row>
-        <Button style={styles.numberButtons} onPress={onNumberPress} title="4" />
-      </Row>
-      <Row>
-        <Button style={styles.numberButtons} onPress={onNumberPress} title="1" />
-      </Row>
-      <Row>
-        <Button style={styles.numberButtons} onPress={onNumberPress} title="." />
-      </Row>
-    </Col>
+  const OperatorButton = ({ title }) => (
+    <Button style={styles.operationsButtons} onPress={onOperatorPress} title={title} />
+  );
 
-    <Col>
-      <Row>
-        <Button style={styles.numberButtons} onPress={onNumberPress} title="8" />
-      </Row>
-      <Row>
-        <Button style={styles.numberButtons} onPress={onNumberPress} title="5" />
-      </Row>
-      <Row>
-        <Button style={styles.numberButtons} onPress={onNumberPress} title="2" />
-      </Row>
-      <Row>
-        <Button style={styles.numberButtons} onPress={onNumberPress} title="0" />
-      </Row>
-    </Col>
+  return (
+    <Grid style={styles.container}>
+      <Col>
+        <Row>
+          <NumberButton title="7" />
+        </Row>
+        <Row>
+          <NumberButton title="4" />
+        </Row>
+        <Row>
+          <NumberButton title="1" />
+        </Row>
+        <Row>
+          <NumberButton title="." />
+        </Row>
+      </Col>
 
-    <Col>
-      <Row>
-        <Button style={styles.numberButtons} onPress={onNumberPress} title="9" />
-      </Row>
-      <Row>
-        <Button style={styles.numberButtons} onPress={onNumberPress} title="6" />
-      </Row>
-      <Row>
-        <Button style={styles.numberButtons} onPress={onNumberPress} title="3" />
-      </Row>
-      <Row>
-        <Button
-          style={styles.numberButtons}
-          onPress={onDelete}
-          onLongPress={onLongDelete}
-          title="DEL"
-        />
-      </Row>
-    </Col>
+      <Col>
+        <Row>
+          <NumberButton title="8" />
+        </Row>
+        <Row>
+          <NumberButton title="5" />
+        </Row>
+        <Row>
+          <NumberButton title="2" />
+        </Row>
+        <Row>
+          <NumberButton title="0" />
+        </Row>
+      </Col>
 
-    <Col>
-      <Row>
-        <Button style={styles.operationsButtons} onPress={onOperatorPress} title="/" />
-      </Row>
-      <Row>
-        <Button style={styles.operationsButtons} onPress={onOperatorPress} title="*" />
-      </Row>
-      <Row>
-        <Button style={styles.operationsButtons} onPress={onOperatorPress} title="-" />
-      </Row>
-      <Row>
-        <Button style={styles.operationsButtons} onPress={onOperatorPress} title="+" />
-      </Row>
-      <Row>
-        <Button style={styles.equalButton} onPress={onResult} title="=" />
-      </Row>
-    </Col>
-  </Grid>
-);
+      <Col>
+        <Row>
+          <NumberButton title="9" />
+        </Row>
+        <Row>
+          <NumberButton title="6" />
+        </Row>
+        <Row>
+          <NumberButton title="3" />
+        </Row>
+        <Row>
+          <Button
+            style={styles.numberButtons}
+            onPress={onDelete}
+            onLongPress={onLongDelete}
+            title="DEL"
+          />
+        </Row>
+      </Col>
+
+      <Col>
+        <Row>
+          <OperatorButton title="/" />
+        </Row>
+        <Row>
+          <OperatorButton title="*" />
+        </Row>
+        <Row>
+          <OperatorButton title="-" />
+        </Row>
+        <Row>
+          <OperatorButton title="+" />
+        </Row>
+        <Row>
+          <Button style={styles.equalButton} onPress={onResult} title="=" />
+        </Row>
+      </Col>
+    </Grid>
+  );
+};
 
 Pad.propTypes = {
   onNumberPress: PropTypes.func.isRequired,
